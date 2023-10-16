ff360e0e72105698fe92cab47f1b192afa464300
Bruno <bruno.hiroyuki@unesp.br>
2023-10-16 01:51:03 -0300

    Toolbar MeusJogos

--- SiteJogosSolution/SiteJogos.Console/Controllers/JogoController.cs
+++ SiteJogosSolution/SiteJogos.Console/Controllers/JogoController.cs
@@ -1,11 +1,25 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using SiteJogos.Core.Entities;
+using SiteJogos.Core.Services.Interface;
 
 namespace SiteJogos.Console.Controllers
 {
     public class JogoController : Controller
     {
+        private readonly UserManager<Usuario> _userManager;
+        private readonly IUsuarioRepository _usuarioRepository;
+
+        public JogoController(UserManager<Usuario> userManager, IUsuarioRepository usuarioRepository)
+        {
+            _userManager = userManager;
+            _usuarioRepository = usuarioRepository;
+        }
+
+        [HttpGet]
         [Authorize]
+        [Route("/MeusJogos")]
         public IActionResult MeusJogos()
         {
             return View();
